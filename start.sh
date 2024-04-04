3835a414d0c241347d4086ac4e8e9611e95a024a
phil-henning <phenning@apple.com>
2024-04-04 10:25:14 -0400

    Removing last created files and updating a few small bits

--- start.sh
+++ start.sh
@@ -1,2 +1,11 @@
+#! /bin/bash
+
+if [[ -f "Alberta-output.txt" ]]; then
+    rm Alberta-output.txt
+fi
+if [[ -f "Ontario-output.txt" ]]; then
+    rm Ontario-output.txt
+fi
+
 source ./env/bin/activate
 python scrub_daddy.py
